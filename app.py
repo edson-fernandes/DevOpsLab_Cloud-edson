6409c272aaf63edcf7b4aaedc978eb3c560703c8
edson-fernandes <69855782+edson-fernandes@users.noreply.github.com>
2023-03-10 01:31:08 -0300

    Bug Removed

--- app.py
+++ app.py
@@ -1,11 +1,23 @@
 
 #REMOVER AS LINHAS
+import unittest
+from flask import Flask
+app = Flask(__name__)
 
-@app.route('/bug')                                                                                                                               
-def bad():                                                                                                                                       
-    try:                                                                                                                                         
-        raise TypeError()                                                                                                                        
-    except TypeError as e:                                                                                                                       
-        print(e)                                                                                                                                 
-    except TypeError as e:                                                                                                                       
+@app.route('/bug')
+def bad():
+    try:
+        raise TypeError()
+    except TypeError as e:
+        print(e)
+    except TypeError as e:
         print("Duplicado, ou seja, nunca vai entrar aqui.")
+
+class TestBugRoute(unittest.TestCase):
+    def test_bad_route(self):
+        with app.test_client() as client:
+            response = client.get('/bug')
+            self.assertEqual(response.status_code, 200)
+
+if __name__ == '__main__':
+    unittest.main()
