8314083549935426ad6b63324ae1a79dc5dcf5ea
edson-fernandes <69855782+edson-fernandes@users.noreply.github.com>
2023-03-10 01:16:50 -0300

    Bug Removed

--- app.py
+++ app.py
@@ -1,23 +1,11 @@
-from flask import Flask
-from flask_wtf.csrf import CSRFProtect
 
-app = Flask(__name__)
+#REMOVER AS LINHAS
 
-csrf = CSRFProtect(app)
-
-@app.route("/")
-def pagina_inicial():
-    return "Laboratório Pipeline DevOps"
-
-@app.route('/bug')                                                                                                                                
-def bad():                                                                                                                                        
-    try:                                                                                                                                          
-        raise TypeError()                                                                                                                         
-    except TypeError as e:                                                                                                                        
-        print(e)                                                                                                                                  
-    except TypeError as e:                                                                                                                        
-        print("Duplicado, ou seja, nunca vai entrar aqui.")    
-
-
-if __name__ == '__main__':
-    app.run()
+@app.route('/bug')                                                                                                                               
+def bad():                                                                                                                                       
+    try:                                                                                                                                         
+        raise TypeError()                                                                                                                        
+    except TypeError as e:                                                                                                                       
+        print(e)                                                                                                                                 
+    except TypeError as e:                                                                                                                       
+        print("Duplicado, ou seja, nunca vai entrar aqui.")
