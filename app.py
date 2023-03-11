b3dad4fa5c6f7f3d4ff90578de0030e4e2e2411f
edson-fernandes <69855782+edson-fernandes@users.noreply.github.com>
2023-03-11 09:23:42 -0300

    Bug Removed

--- app.py
+++ app.py
@@ -1,23 +1,11 @@
 
-import unittest
 from flask import Flask
+from flask_wtf.csrf import CSRFProtect
+
 app = Flask(__name__)
 
-@app.route('/bug')
-def bad():
-    try:
-        raise TypeError()
-    except TypeError as e:
-        return str(e), 500
+csrf = CSRFProtect(app)
 
-class TestBugRoute(unittest.TestCase):
-    def setUp(self):
-        self.client = app.test_client()
-
-    def test_bad_route(self):
-        response = self.client.get('/bug')
-        self.assertEqual(response.status_code, 500)
-        self.assertIn(b"TypeError", response.data)
-
-if __name__ == '__main__':
-    unittest.main()
+@app.route("/")
+def pagina_inicial():
+    return "Laboratório Pipeline DevOps"
