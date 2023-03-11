49618dbb6b858db96f36ddbe8b635015f9cce4e0
edson-fernandes <69855782+edson-fernandes@users.noreply.github.com>
2023-03-11 09:26:10 -0300

    Bug Removed

--- test.py
+++ test.py
@@ -1,14 +1,19 @@
+# -*- coding: utf-8 -*-
 from app import app
 import unittest
 
-class TestBugRoute(unittest.TestCase):
+class Test(unittest.TestCase):
     def setUp(self):
+        # cria uma instância do unittest, precisa do nome "setUp"
         self.app = app.test_client()
-        self.result = self.app.get('/bug')
 
-    def test_bug_route(self):
-        self.assertEqual(self.result.status_code, 500) # O TypeError resulta em um erro interno do servidor (500)
+        # envia uma requisicao GET para a URL
+        self.result = self.app.get('/')
 
-    def test_error_message(self):
-        self.assertIn(b"TypeError", self.result.data) # Espera-se que a mensagem de erro apareça na resposta
-        self.assertNotIn(b"Duplicado", self.result.data) # Garante que a mensagem de duplicidade não apareça na resposta
+    def test_requisicao(self):
+        # compara o status da requisicao (precisa ser igual a 200)
+        self.assertEqual(self.result.status_code, 200)
+
+    def test_conteudo(self):
+        # verifica o retorno do conteudo da pagina
+        self.assertEqual(self.result.data.decode('utf-8'), "Laboratório Pipeline DevOps")
